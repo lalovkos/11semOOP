ef2fdc67df6185243f0e8dea6542f782247f4600
lalovkos <52522496+lalovkos@users.noreply.github.com>
2021-12-03 14:02:07 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11semOOP
+{
+
+    struct Point { }
+    struct Color { }
+
+    interface IRenderer
+    {
+        void DrawPolygon(IEnumerable<Point> points, Color color);
+        void FillPolygon(IEnumerable<Point> points, Color color,
+            Color fillColor);
+        void DrawText(string text, Point origin, Color color);
+    }
+
+    interface IDrawable
+    {
+        void Draw(IRenderer render);
+        bool IsInside(Point point);
+    }
+
+    interface IShape
+    {
+        IEnumerable<Point> Points { get; }
+        void UpdatePoints(ITransform transform);
+    }
+
+    interface ICaption
+    {
+        string Text { get; set; }
+    }
+
+    interface ITransform
+    {
+        Point Transform(Point p);
+    }
+
+    abstract class ShapeFabric
+    {
+        protected static ShapeFabric Current { get; set; }
+        protected abstract IShape InternalCreateShape(string json);
+        public static IShape CreateShape(string json)
+        {
+            return Current.InternalCreateShape(json);
+        }
+    }
+
+    class SuperShapeFabric : ShapeFabric
+    {
+        protected override IShape InternalCreateShape(string json)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SetCurrent()
+        {
+            Current = new SuperShapeFabric();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
